bb4148469a54e266dd169a35bb6d4a6e216eaffd
chaguidiao <chaguidiao@gmail.com>
2024-12-30 17:49:28 +0800

    Initial checkin

---
+++ Fluid.py
@@ -0,0 +1,254 @@
+
+import numpy as np
+from scipy.linalg import solve
+from scipy.sparse.linalg import spsolve
+from scipy.sparse import csc_matrix, coo_matrix, issparse
+from scipy.interpolate import RegularGridInterpolator
+
+class Fluid(object):
+    def __init__(self,
+                 N=64,
+                 dt=0.1,
+                 diff=0.01,
+                 visc=0.01,
+                 force=5.0,
+                 dens_source=100.0,
+                 solver='iter'):
+        self.N = N
+        self.dt = dt
+        self.diff = diff
+        self.visc = visc
+        self.force = force
+        self.dens_source = dens_source
+        self.solver = solver
+        # TODO: Extend to > 2d support
+        self.v = np.zeros((self.N + 2, self.N + 2, 2))
+        self.dens = np.zeros((self.N + 2, self.N + 2, 1))
+        self.coeff_density = self._build_density_coeff()
+        self.coeff_velocity = self._build_velocity_coeff()
+        self.coeff_project = self._build_project_coeff()
+
+    def _build_Amat(self):
+        # TODO extend to > 2d support
+        A = np.fromfunction(self._build_matrix, [self.N] * 4)
+        # Convert to 2d matrix for linear solve
+        A = A.reshape(A.shape[0] ** 2, -1)
+        A = A.astype('int8')
+        A[0] = 0
+        A[-1] = 0
+        A[..., 0] = 0
+        A[..., -1] = 0
+        return A
+
+
+    @staticmethod
+    # TODO: Extend to > 2d support
+    def _build_matrix(x, y, i, j):
+        return ((x == i + 1) & (y == j)) | \
+               ((x == i - 1) & (y == j)) | \
+               ((x == i) & (y == j + 1)) | \
+               ((x == i) & (y == j - 1))
+
+    def _build_density_coeff(self):
+        '''
+        x0 = x - a(xt + xb + xl + xr - 4x)
+        x0 = (1+4a)x - axt - axb - axl - axr
+        Ax = x0
+        '''
+        A = self._build_Amat()
+        a = self.dt * self.diff * pow(self.N, 2)
+        coeff = -a * A + np.eye(A.shape[0]) * (1 + 4 * a)
+        return csc_matrix(coeff)
+
+    def _build_velocity_coeff(self):
+        '''
+        x0 = x - a(xt + xb + xl + xr - 4x)
+        x0 = (1+4a)x - axt - axb - axl - axr
+        Ax = x0
+        '''
+        A = self._build_Amat()
+        a = self.dt * self.visc * pow(self.N, 2)
+        coeff = -a * A + np.eye(A.shape[0]) * (1 + 4 * a)
+        return csc_matrix(coeff)
+
+    def _build_project_coeff(self):
+        '''
+        x0 = 4x - xt - xb - xl - xr
+        '''
+        A = self._build_Amat()
+        coeff = -A + np.eye(A.shape[0]) * 4
+        return csc_matrix(coeff)
+
+    def _itsolve(self, x, x0, N, a, c):
+        iter_method = 'jacobi'
+        for k in range(0, 20):
+            # Jacobi method rather than gauss siedel method used
+            # by in the original c implementation
+            if iter_method == 'jacobi':
+                x[1:N + 1, 1:N + 1] = (x0[1:N + 1, 1:N + 1] + a *
+                                       (x[0:N, 1:N + 1] +
+                                       x[2:N + 2, 1:N + 1] +
+                                       x[1:N + 1, 0:N] +
+                                       x[1:N + 1, 2:N + 2])) / c
+            else: # gauss siedel
+                for i in range(1, N+1):
+                    for j in range(1, N+1):
+                        x[i, j] = (x0[i, j] + a * (x[i-1, j] + x[i+1, j] + x[i, j-1] + x[i, j+1])) / c
+            #print(f'k: {k}')
+            #print(x[:, :, 0])
+
+            #if k < 4:
+            #   print(f'--before bound k: {k}')
+            #   print(x[:, :, 0])
+
+            self._set_bound(x)
+
+            #if k < 4:
+            #   print(f'--after bound k: {k}')
+            #   print(x[:, :, 0])
+
+        return x
+
+    @staticmethod
+    def _solve(X, N, coeff):
+        # TODO > 2d shape generalize
+        X_inner = X[1:N+1, 1:N+1]
+        X_inner_ori_shape = X_inner.shape
+        X_inner = csc_matrix(X_inner.reshape(coeff.shape[0], -1))
+        X_inner = spsolve(coeff, X_inner)
+        if issparse(X_inner): X_inner = np.array(X_inner.todense()) # TODO
+        X[1:N+1, 1:N+1] = X_inner.reshape(X_inner_ori_shape)
+        return X
+
+    def _advect(self, X):
+        # linear backtrace
+        # TODO How to extend to RK2 solver as mentioned in the paper
+        coords = [coord[..., np.newaxis] for coord in np.meshgrid(*[np.arange(self.N)] * (X.ndim - 1), indexing='ij')]
+        coords = np.concat(coords, axis=X.ndim - 1)
+        #coords = np.einsum('ij...->ji...', coords)
+        coords = coords + 1.0
+        transport = coords - self.dt * self.N * self.v[1:-1,1:-1,:] # Leaving out boundary cells
+        transport[transport < 0.5] = 0.5
+        transport[transport > (self.N + 0.5)] = self.N + 0.5
+        grids = list((np.arange(i) for i in X.shape[:-1]))
+        interp = RegularGridInterpolator(grids, X)
+        X[1:-1,1:-1,:] = interp(transport)
+        return X
+
+    def add_density_source(self, source):
+        self.dens += source[..., [0]] * self.dt
+
+    def diffuse_density(self):
+        if self.solver == 'direct':
+            self.dens = self._solve(self.dens, self.N, self.coeff_density)
+        else:
+            a = self.dt * self.diff * self.N * self.N
+            c = 1 + 4 * a
+            self.dens = self._itsolve(self.dens.copy(), self.dens, self.N, a, c)
+
+    def advect_density(self):
+        self.dens = self._advect(self.dens)
+
+    @staticmethod
+    def _set_bound(x):
+        # Processing along the edges first
+        x[0] = x[1]
+        x[-1] = x[-2]
+        x[:, 0] = x[:, 1]
+        x[:, -1] = x[:, -2]
+        # Velocity bounces back after hitting the wall
+        # Assuming density is scalar so there should not be ambiguity here
+        # TODO How to generalize to 3D and higher dimensional
+        if x.shape[-1] > 1:
+            # u component
+            x[0, ..., 0] = -x[0, ..., 0]
+            x[-1, ..., 0] = -x[-1, ..., 0]
+            # v component
+            x[..., 0, 1] = -x[..., 0, 1]
+            x[..., -1, 1] = -x[..., -1, 1]
+            '''
+            # u component
+            x[..., 0, 0] = -x[..., 0, 0]
+            x[..., -1, 0] = -x[..., -1, 0]
+            # v component
+            x[0, ..., 1] = -x[0, ..., 1]
+            x[-1, ..., 1] = -x[-1, ..., 1]
+            '''
+        # Lastly, special handling for the four corners
+        x[0, 0] = 0.5 * (x[0, 1] + x[1, 0])
+        x[-1, 0] = 0.5 * (x[-1, 1] + x[-2, 0])
+        x[0, -1] = 0.5 * (x[0, -2] + x[1, -1])
+        x[-1, -1] = 0.5 * (x[-2, -1] + x[-1, -2])
+        return x
+
+    def set_density_bound(self):
+        self.dens = self._set_bound(self.dens)
+
+    def update_density(self, source):
+        self.add_density_source(source)
+        self.diffuse_density()
+        self.set_density_bound()
+        self.advect_density()
+        self.set_density_bound()
+
+    def get_density(self):
+        return self.dens
+
+    def set_velocity_bound(self):
+        self.v = self._set_bound(self.v)
+
+    def add_velocity_source(self, source):
+        self.v += source[..., 1:] * self.dt
+
+    def diffuse_velocity(self):
+        if self.solver == 'direct':
+            self.v = self._solve(self.v, self.N, self.coeff_velocity)
+        else:
+            a = self.dt * self.visc * self.N * self.N
+            c = 1 + 4 * a
+            self.v = self._itsolve(self.v.copy(), self.v, self.N, a, c)
+
+    def advect_velocity(self):
+        # Self advection
+        self.v = self._advect(self.v)
+
+    def project_velocity(self):
+        v_dim = self.v.shape[-1]
+        gradient_components = np.gradient(self.v, axis=np.arange(v_dim))
+        # divergence is scalar
+        # No need to divide 0.5 again as compared with the original implementation
+        # Because np.gradient already divide by half
+        divergence = np.add.reduce([m[..., i] for i, m in enumerate(gradient_components)])
+        divergence = -divergence[..., np.newaxis] / self.N
+        divergence = self._set_bound(divergence)
+        #print('before')
+        #print(divergence[:, :, 0])
+        if self.solver == 'direct':
+            divergence = self._solve(divergence, self.N, self.coeff_project)
+        else:
+            a = 1.
+            c = 4.
+            divergence = self._itsolve(np.zeros(divergence.shape), divergence, self.N, a, c)
+        #print('after')
+        #print(divergence[:, :, 0])
+        p = np.gradient(divergence, axis=np.arange(v_dim))
+        p = np.stack(p, axis=v_dim).squeeze()
+        self.v = self.v - (self.N * p)
+        self.v = self._set_bound(self.v)
+
+    def update_velocity(self, source):
+        self.add_velocity_source(source)
+        self.diffuse_velocity()
+        self.project_velocity()
+        self.set_velocity_bound()
+        self.advect_velocity()
+        self.project_velocity()
+        self.set_velocity_bound()
+
+    def get_velocity(self):
+        return self.v
+
+    def reset(self):
+        self.dens = np.zeros(self.dens.shape)
+        self.v = np.zeros(self.v.shape)
+
---
+++ fluids_anim.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env /media/nfs/sda3/workspace/Venvs/socrates/graphic/bin/python
+
+from raylib import *
+from Fluid import *
+
+def draw_density(dens, window_width, window_height, cell_size):
+    z = dens.copy()
+    #z *= (255.0 / np.fmax((z.max() - z.min()), 0.001))
+    z *= 255.0
+    z = z.astype('uint8')
+    x = np.linspace(0, window_width, z.shape[0])
+    y = np.linspace(0, window_height, z.shape[1])
+    x, y = np.meshgrid(x, y)
+    rlBegin(RL_QUADS)
+    for i in range(z.shape[0] - 1):
+        for j in range(z.shape[1] - 1):
+            _z = z[i, j].item()
+            rlColor4ub(*([_z]*3) + [255])
+            rlVertex2f(x[i, j], y[i, j])
+            _z = z[i+1, j].item()
+            rlColor4ub(*([_z]*3) + [255])
+            rlVertex2f(x[i+1, j], y[i+1, j])
+            _z = z[i+1, j+1].item()
+            rlColor4ub(*([_z]*3) + [255])
+            rlVertex2f(x[i+1, j+1], y[i+1, j+1])
+            _z = z[i, j+1].item()
+            rlColor4ub(*([_z]*3) + [255])
+            rlVertex2f(x[i, j+1], y[i, j+1])
+    rlEnd()
+
+
+def draw_velocity(v, window_width, window_height, cell_size):
+    h = 1.0 / cell_size
+    x = np.linspace(-0.5, cell_size - 0.5, cell_size + 1) * h
+    y = np.linspace(-0.5, cell_size - 0.5, cell_size + 1) * h
+    x, y = np.meshgrid(x, y)
+    red = ffi.new('struct Color *', [255, 0, 0, 255])[0]
+    for i in range(1, v.shape[0] - 2):
+        for j in range(1, v.shape[1] - 2):
+            xstart = x[i, j] * window_width
+            ystart = y[i, j] * window_height
+            xend = (x[i, j] + v[i, j, 0]) * window_width
+            yend = (y[i, j] + v[i, j, 1]) * window_height
+            xstart, ystart, xend, yend = int(xstart), int(ystart), int(xend), int(yend)
+            DrawLine(xstart, ystart, xend, yend, red)
+
+
+def get_source_from_UI(source, window_width, window_height, dens_source, force, omx, omy):
+    if IsMouseButtonDown(MOUSE_BUTTON_RIGHT):
+        vec = GetMousePosition()
+        x = int(vec.x / window_width * source.shape[1]) # Column for width
+        y = int(vec.y / window_height * source.shape[0])
+        source[y, x, 0] = dens_source
+        omx, omy = x, y
+    elif IsMouseButtonDown(MOUSE_BUTTON_LEFT):
+        vec = GetMousePosition()
+        x = int(vec.x / window_width * source.shape[1]) # Column for width
+        y = int(vec.y / window_height * source.shape[0])
+        source[y, x, 1] = force * (x - omx)
+        source[y, x, 2]  = force * (omy - y)
+        omx, omy = x, y
+
+    return omx, omy
+
+def main():
+    window_width = 800
+    window_height = 450
+    N = 64
+    show_velocity = False
+    omx, omy = 0, 0
+
+    fluid = Fluid(N=N, force=15.)
+    # 1st is density, 2nd and 3rd are velocity components
+    # respectively
+    source = np.zeros((N + 2, N + 2, 3))
+
+    # Debug 1
+    #source = np.random.choice([0, 1], (N + 2) * (N + 2) * 3, p=[0.9, 0.1])
+    #source = source.reshape(N+2, N+2, 3)
+    #ource = source * np.random.rand(*source.shape)
+
+    # Debug 2
+    #source[35, 35, 0] = 100
+    #source[35, 10, 1] = 17.7
+    #source[35, 10, 2] = -88.6
+
+    InitWindow(window_width, window_height, b"Fluid dynamics the numpy way")
+    SetWindowState(FLAG_WINDOW_RESIZABLE)
+    SetTargetFPS(60)
+    while not WindowShouldClose():
+        omx, omy = get_source_from_UI(source, GetScreenWidth(), GetScreenHeight(), fluid.dens_source, fluid.force, omx, omy)
+        if IsKeyPressed(KEY_V):
+            show_velocity = not show_velocity
+        if IsKeyPressed(KEY_C):
+            fluid.reset()
+        fluid.update_velocity(source)
+        fluid.update_density(source)
+        BeginDrawing()
+        ClearBackground(BLACK)
+        if show_velocity:
+            draw_velocity(fluid.get_velocity(), GetScreenWidth(), GetScreenHeight(), N)
+        else:
+            draw_density(fluid.get_density(), GetScreenWidth(), GetScreenHeight(), N)
+        EndDrawing()
+        source = np.zeros((N + 2, N + 2, 3))
+    CloseWindow()
+
+
+if __name__ == '__main__':
+    main()
