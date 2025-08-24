924f8566aced984c0de91cee1abae1f90330e031
chaguidiao <chaguidiao@gmail.com>
2025-08-25 01:03:18 +0800

    import fix

--- collective_motion/main.py
+++ collective_motion/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.widgets import RadioButtons, Slider
-from vicsek_class import VicsekModel
+from vicsek import VicsekModel
 
 # --- Model Parameters ---
 BOX_SIZE = 100
